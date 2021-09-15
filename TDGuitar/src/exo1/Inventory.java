9e8041d3e36d7c4e5e7e2371844cf51fc5c9b8cb
PL <pierre.laforcade@univ-lemans.fr>
2021-09-15 09:10:43 +0200

    add V1 and V2 corrections for the Guitar subject

---
+++ TDGuitar/src/exo1/Inventory.java
@@ -0,0 +1,69 @@
+package exo1;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+import exo2.Guitar;
+
+public class Inventory {
+
+	private List<Guitar> guitars;
+
+	public Inventory() {
+		guitars = new LinkedList<>();
+	}
+
+	public void addGuitar(String serialNumber, double price, String builder, String model, String type, String backWood,
+			String topWood) {
+
+		guitars.add(new Guitar(serialNumber, price, builder, model, type, backWood, topWood));
+	}
+
+	public Guitar getGuitar(String serialNumber) {
+		for (Iterator<Guitar> i = guitars.iterator(); i.hasNext();) {
+			Guitar guitar = i.next();
+			if (guitar.getSerialNumber().equals(serialNumber)) {
+				return guitar;
+			}
+		}
+		return null;
+	}
+
+	public Guitar search(Guitar searchGuitar) {
+		for (Iterator<Guitar> i = guitars.iterator(); i.hasNext();) {
+			Guitar guitar = i.next();
+			
+			if (noMatch(searchGuitar.getBuilder(), guitar.getBuilder())) {
+				continue;
+			}
+			if (noMatch(searchGuitar.getModel(), guitar.getModel())) {
+				continue;
+			}
+			if (noMatch(searchGuitar.getType(), guitar.getType())) {
+				continue;
+			}
+			if (noMatch(searchGuitar.getBackWood(), guitar.getBackWood())) {
+				continue;
+			}
+			if (noMatch(searchGuitar.getTopWood(), guitar.getTopWood())) {
+				continue;
+			}
+	
+			return guitar;
+		}
+		return null;
+	}
+
+	private boolean noMatch(String searchedProp, String realProp) {
+		if (searchedProp == null)
+			return false;
+		if (searchedProp.equals(""))
+			return false;
+		if (searchedProp.equals(realProp))
+			return false;
+		
+		return true;
+	}
+
+}
---
+++ TDGuitar/src/exo2/Inventory.java
@@ -0,0 +1,70 @@
+package exo2;
+
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.LinkedList;
+import java.util.List;
+
+public class Inventory {
+
+	private List<Guitar> guitars;
+
+	public Inventory() {
+		guitars = new LinkedList<>();
+	}
+
+	public void addGuitar(String serialNumber, double price, 
+			Builder builder, String model, Type type, Wood backWood,
+			Wood topWood) {
+
+		guitars.add(new Guitar(serialNumber, price, builder, model, type, backWood, topWood));
+	}
+
+	public List<Guitar> search(Guitar searchGuitar) {
+		List<Guitar> matchingGuitars = new ArrayList<>();
+		
+		for (Iterator<Guitar> i = guitars.iterator(); i.hasNext();) {
+			Guitar guitar = i.next();
+			
+			if (noMatch(searchGuitar.getBuilder(), guitar.getBuilder())) {
+				continue;
+			}
+			if (noMatch(searchGuitar.getModel(), guitar.getModel())) {
+				continue;
+			}
+			if (noMatch(searchGuitar.getType(), guitar.getType())) {
+				continue;
+			}
+			if (noMatch(searchGuitar.getBackWood(), guitar.getBackWood())) {
+				continue;
+			}
+			if (noMatch(searchGuitar.getTopWood(), guitar.getTopWood())) {
+				continue;
+			}
+	
+			matchingGuitars.add(guitar);
+		}
+		return matchingGuitars;
+	}
+
+	private boolean noMatch(Enum searchedProp, Enum realProp) {
+		if (searchedProp == null)
+			return false;
+		if (searchedProp.equals(realProp))
+			return false;
+		
+		return true;
+	}
+	
+	private boolean noMatch(String searchedProp, String realProp) {
+		if (searchedProp == null)
+			return false;
+		if (searchedProp.equals(""))
+			return false;
+		if (searchedProp.equalsIgnoreCase(realProp))
+			return false;
+		
+		return true;
+	}
+
+}
